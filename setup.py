75ff4a69af0b5e7da6f073498bae1ac5e448fbc7
Rui W <wrhlh369@gmail.com>
2024-06-02 19:20:12 -0400

    123123

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='folder2',
+    name='folder1',
     version='0.0.1',
     description='test package',
     url='git@github.com:RaymRaym/exp_pymodule.git',
